1e6a4e3fb2f499c24966fdb2a5d13435f65fbe1b
gtDataLab <gregg.tyson@gmail.com>
2018-04-19 10:53:36 -0400

    Updated package.json (installed webpack-cli) and added notes to gulp build.js

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -7,8 +7,10 @@ cssnano = require('gulp-cssnano'),
 uglify = require('gulp-uglify'),
 browserSync = require('browser-sync').create();
 
-var outputDir = "docs";
+var outputDir = "docs";  // use 'docs' when uploading to GitHub pages
+//var outputDir = "dist"; // use 'dist' normally
 
+// Opens the new distribution in a browser
 gulp.task('previewDist', function() {
 	browserSync.init({
 		notify: false,
@@ -18,10 +20,13 @@ gulp.task('previewDist', function() {
 	});
 });
 
+// Deletes the existing output folder; do this before launching the other tasks
+// This task runs the icons task to ensure we have an icons sprite before regenerating the CSS with the styles task
 gulp.task('deleteDistFolder', ['icons'], function() {
 	return del("./" + outputDir);
 });
 
+// Copies any other files/folders for the app; ensure you exclude (!) the files you don't want to be part of this task
 gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
 	var pathsToCopy = [
 		'./app/**/*',
@@ -36,6 +41,8 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
 	.pipe(gulp.dest("./" + outputDir));
 });
 
+// Optimize images
+// Ensure you exclude the icons if you're using an icon sprite
 gulp.task('optimizeImages', ['deleteDistFolder'], function() {
 	return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
 	.pipe(imagemin({
@@ -46,10 +53,14 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
 	.pipe(gulp.dest("./" + outputDir + "/assets/images"));
 });
 
+// Ensure usemin doesn't kick off until the delete previous folder task finishes
+// (which will ensure we have the newest icons sprite -- since that's a dependency in the delete task)
 gulp.task('useminTrigger', ['deleteDistFolder'], function() {
 	gulp.start("usemin");
 });
 
+// Compress and minify CSS and JSS
+// First run styles and scripts task to get a fresh copy of those files
 gulp.task('usemin', ['styles', 'scripts'], function() {
 	return gulp.src("./app/index.html")
 	.pipe(usemin({
@@ -59,4 +70,5 @@ gulp.task('usemin', ['styles', 'scripts'], function() {
 	.pipe(gulp.dest("./" + outputDir));
 });
 
+// Full build task 
 gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
